7c39fb8b170ba9f6432c092cefff4c4ba17edc1b
Jayzee <joker_hjz@hotmail.com>
2017-12-28 22:51:28 +1300

    Create example.php

---
+++ example.php
@@ -0,0 +1,16 @@
+    $IPayNow = new IPayNow();
+	$IPayNow->setApiId("abcdefghijklmnopqrstuvwxyz"); //Customise or write on cofig
+	$IPayNow->setSecretKey("1234657890"));
+	$IPayNow->setRedirectUrl("https://www.ipaynow.cn");
+	$IPayNow->setNotifyUrl("https://www.ipaynow.cn");
+	$IPayNow->setDisplayName("test");
+	$IPayNow->setOrderDescription("test");
+	$IPayNow->setOrderNumber("19847612516413131");
+	$IPayNow->setProductDetail([
+		{'name' => 'testProduct1', 'quantity' => 1},
+		{'name' => 'testProduct2', 'quantity' => 2}
+    ]);
+	$IPayNow->setOrderAmount(650.00);
+	$IPayNow->setCurrency('NZD');
+	$IPayNow->Alipay();
+	echo ($IPayNow->checkout()); //Rediect to this Url 重定向到这个链接
